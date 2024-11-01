6afa337191a32ab1b0da59f498e76255414e1495
WALKER <39916348+walker-hyf@users.noreply.github.com>
2024-11-01 15:57:28 +0800

    Add files via upload

--- tools/my_utils.py
+++ tools/my_utils.py
@@ -9,7 +9,7 @@ def load_audio(file, sr):
         # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
         file = (
             file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
-        ) 
+        )  
         out, _ = (
             ffmpeg.input(file, threads=0)
             .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sr)
